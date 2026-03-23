e7beea3395898b1872b4fdfcd1f13ddf49482e95
PhatNhuLe2003 <phatleschool@gmail.com>
2026-03-22 21:07:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
 
+
+
+function showDate()     
+        {
+            const d = new Date();
+            document.getElementById("today").innerHTML =
+                d.toLocaleDateString('en-US', 
+                {
+                    weekday: 'long',
+                    month: 'long',
+                    day: 'numeric',
+                    year: 'numeric'
+                }
+            );
+        }
+        function validatePassword()     
+            {
+                const pw = document.querySelector('input[name="password"]').value;
+                const cpw = document.querySelector('input[name="confirm_password"]').value;
+
+                        if (pw !== cpw) 
+                        {
+                          alert("Passwords do not match.");
+                          return false;
+                        }
+                        return true;
+  }
+     function formatDOB()
+     {
+         const dobInput = document.getElementById("dob");
+         if (!dobInput.value) return true;
+
+         const parts = dobInput.value.split("-");
+         if (parts.length !==3)
+         {
+             alert("Please enter Date of Birth in MM/DD/YYYY format.");
+             return false;
+         }
+
+         const yyyy = parts[0];
+         const mm = parts[1];
+         const dd = parts[2];
+
+         dobInput.type = "text";
+         dobInput.value = `${mm}/${dd}/${yyyy}`;
+
+         return true;
+     }
+    function validateForm()
+        {
+            return validatePassword() && formatDOB();
+        }
